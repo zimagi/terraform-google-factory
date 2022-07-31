8e41d3ab8f169a4df1454e803b39df3aca283d31
jagyugyaerik <jagyugyaerik@gmail.com>
2022-08-01 01:14:39 +0200

    Uncomment private pool

--- modules/terraform-google-private-pool/main.tf
+++ modules/terraform-google-private-pool/main.tf
@@ -4,45 +4,45 @@ resource "google_compute_network" "default" {
   auto_create_subnetworks = false
 }
 
-# resource "google_compute_global_address" "default" {
-#   project = var.project_id
+resource "google_compute_global_address" "default" {
+  project = var.project_id
 
-#   name          = var.global_address_name
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = var.global_address_prefix_length
-#   address       = var.global_address
-#   network       = google_compute_network.default.id
-# }
+  name          = var.global_address_name
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = var.global_address_prefix_length
+  address       = var.global_address
+  network       = google_compute_network.default.id
+}
 
-# resource "google_service_networking_connection" "default" {
-#   network                 = google_compute_network.default.id
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.default.name]
-# }
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.default.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.default.name]
+}
 
-# resource "google_cloudbuild_worker_pool" "pool" {
-#   project = var.project_id
+resource "google_cloudbuild_worker_pool" "pool" {
+  project = var.project_id
 
-#   name          = var.pool_name
-#   location      = var.pool_location
+  name          = var.pool_name
+  location      = var.pool_location
 
-#   worker_config {
-#     disk_size_gb = var.disk_size_gb
-#     machine_type = var.machine_type
-#     no_external_ip = var.no_external_ip
-#   }
+  worker_config {
+    disk_size_gb = var.disk_size_gb
+    machine_type = var.machine_type
+    no_external_ip = var.no_external_ip
+  }
 
-#   network_config {
-#     peered_network = google_compute_network.default.id
-#   }
-# }
+  network_config {
+    peered_network = google_compute_network.default.id
+  }
+}
 
-# resource "null_resource" "update_peering" {
-#   depends_on = [google_service_networking_connection.default]
+resource "null_resource" "update_peering" {
+  depends_on = [google_service_networking_connection.default]
 
-#   provisioner "local-exec" {
-#     command     = "gcloud compute networks peerings update servicenetworking-googleapis-com --network=${google_compute_network.default.name} --export-custom-routes --no-export-subnet-routes-with-public-ip --project ${var.project_id}"
-#     on_failure  = fail
-#   }
-# }
+  provisioner "local-exec" {
+    command     = "gcloud compute networks peerings update servicenetworking-googleapis-com --network=${google_compute_network.default.name} --export-custom-routes --no-export-subnet-routes-with-public-ip --project ${var.project_id}"
+    on_failure  = fail
+  }
+}
